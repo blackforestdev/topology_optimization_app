71f401c0e7b9ae7f58d3f044b2941dec6f84971d
blackforestdev <6.28x10e18@gmail.com>
2024-05-30 02:32:18 -0700

    removed redunden code from main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import sys
 from PyQt5.QtWidgets import QApplication
 from gui.app import TopologyOptimizationApp
+import sys
 
 def main():
-    app = QApplication(sys.argv) # Create a Qt application
+    app = QApplication(sys.argv)  # Create a Qt application
     app.setStyle("Fusion")
-    mainWindow = TopologyOptimizationApp() # Initialize the main window
-    mainWindow.show() # Show the main window
+    window = TopologyOptimizationApp()  # Initialize the main window
+    window.show()  # Show the main window
     sys.exit(app.exec_())  # Start the application event loop
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TopologyOptimizationApp()
-    window.show()
-    sys.exit(app.exec_())
+    main()
